78450569e2fb2101cf0c7a2da1a6c0c0f4afed10
lopezalvaro16 <alvaro154020747@gmail.com>
2023-08-19 13:55:43 -0300

    Subiendo ejercicio 6 React intro parte 1

--- 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ 06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,32 @@
 import React from "react";
+import Botones from "./Botones";
 
-const studentName = "";
+const studentName = "Alvaro";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
-  // el código de tu componente acá
-  return;
+  // Definimos handleClick como una función
+  const handleClick = (mensaje) => {
+    alert(mensaje);
+  };
+
+  return (
+    <div>
+      <h1>Hola</h1>
+      <h3>{studentName}</h3>
+      <ul>
+        {techSkills.map((skill, index) => (
+          <li key={index}>{skill}</li>
+        ))}
+      </ul>
+      {/* Pasamos las funciones de manejo de clics como propiedades */}
+      <Botones
+        onClickM1={() => handleClick(alerts.m1)}
+        onClickM2={() => handleClick(alerts.m2)}
+      />
+    </div>
+  );
 }
 
 // Esto lo exportamos para los tests
---
+++ 06-React-Intro/homework/01 - Exercises/src/components/Botones.jsx
@@ -0,0 +1,10 @@
+function Botones({ onClickM1, onClickM2 }) {
+  return (
+    <div>
+      <button onClick={onClickM1}>Modulo 1</button>
+      <button onClick={onClickM2}>Modulo 2</button>
+    </div>
+  );
+}
+
+export default Botones;
