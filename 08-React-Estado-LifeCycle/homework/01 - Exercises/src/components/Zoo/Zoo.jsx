c5c8d2a36946e33da53cf2f55052fc180fc4f5c2
lopezalvaro16 <alvaro154020747@gmail.com>
2023-08-28 10:42:43 -0300

    Ejercicio de redux

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // eslint-disable-next-line no-unused-vars
 import Animals from "../Animals/Animals";
 // eslint-disable-next-line no-unused-vars
@@ -7,11 +7,45 @@ import Species from "../Species/Species";
 
 export default function Zoo() {
   /* Escribe acá tu código */
+  useEffect(() => {
+    fetch("http://localhost:3001/zoo")
+      .then((res) => res.json())
+      .then((data) => {
+        setZoo({
+          ...zoo,
+          animals: data.animals,
+          species: data.species,
+          allAnimals: data.animals,
+        });
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
+  const [zoo, setZoo] = useState({
+    zooName: "",
+    Animals: [],
+    species: [],
+    allAnimals: [],
+  });
+
+  const handleSpecies = (evento) => {};
+  const handleAllSpecies = () => {};
+  const handleInputChange = (evento) => {
+    const nuevoNombre = evento.target.value;
+    setZoo({
+      Animals: zoo.Animals,
+      species: zoo.species,
+      allAnimals: zoo.allAnimals,
+      zooName: nuevoNombre,
+    });
+  };
 
   return (
     <div>
-      <h1>-</h1>
+      <label htmlFor="">Zoo Name</label>
+      <input onChange={handleInputChange} value={zoo.zooName} />
+      <h1>{zoo.zooName}</h1>
+      <Animals />
     </div>
   );
 }
--- 11-Redux/homework/01 - Exercises/src/index.js
+++ 11-Redux/homework/01 - Exercises/src/index.js
@@ -3,10 +3,10 @@ const contador = require("./reducer");
 const { incremento, decremento } = require("./actions");
 
 // En esta línea creamos nuestro store. Pasándole como parámetro nuestro Reducer
-var store;
+var store = createStore(contador);
 
 // Obtenemos el elemento con el id `valor`.
-var valor;
+var valor = document.querySelector("#valor");
 
 // Esta función nos va a servir para actualizar nuestro DOM con el valor que tengamos en nuestro Store.
 // En el primer render y cada vez que nos subscribamos al Store.
@@ -14,11 +14,41 @@ var valor;
 function renderContador() {
   // Obtenemos la propiedad 'contador' de nuestro store:
   // Seteamos el número obtenido como texto dentro del elemento con id 'valor':
+
+  let counter = store.getState().contador;
+
+  valor.innerHTML = counter;
 }
 
 // Ejecutamos la función 'renderContador':
 
+renderContador();
+
 // Nos subscribimos al store pasándole la misma función. Así cada vez que llegue una acción, ejecutamos la función:
+
+store.subcribe(renderContador);
 
 // Por último, utilizamos los botones de nuestro HTML para que cada vez que hagamos click,
 // hagan un dispatch al store de la acción correspondiente:
+
+let botonIncremento = document.getElementById("incremento");
+let botonDecremento = document.getElementById("decremento");
+let botonImpar = document.getElementById("incrementoImpar");
+let botonAsync = document.getElementById("incrementoAsync");
+
+botonIncremento.addEventListener("click", () => {
+  store.dispatch(incremento(1));
+});
+botonDecremento.addEventListener("click", () => {
+  store.dispatch(decremento());
+});
+botonImpar.addEventListener("click", () => {
+  if (store.getState().contador % 2 !== 0) {
+    store.dispatch(incremento(1));
+  }
+});
+botonAsync.addEventListener("click", () => {
+  setTimeout(() => {
+    store.dispatch(incremento(1));
+  }, 2000);
+});
